41443fd443218117549a8070bf3e15933e470cf9
Adama Cissé <adam.cis345@gmail.com>
2022-12-03 23:53:41 +0100

    programme d'authentification fonctionnel

--- authentification/app/views.py
+++ authentification/app/views.py
@@ -19,28 +19,44 @@ def register(request):
         email = request.POST['email']
         password = request.POST['password']
         password1 = request.POST['password1']
-        my_user = User.objects.create(username, email, password)
-        my_user.firt_name = firstname
-        my_user.lastt_name = lastname
+        if User.objects.filter(username=username):
+            messages.error(request, "Ce nom d'utilisateur est indisponible")
+            return redirect('register')
+        if User.objects.filter(email=email):
+            messages.error(request, "Cette email est déja utilisé")
+            return redirect('register')
+        if not username.isalnum():
+            messages.error(request, "Le username doit être alpha numérique")
+            return redirect('register')
+        if password != password1:
+            messages.error(request, "Les mots de passe ne sont pas identiques")
+            return redirect('register')
+
+        my_user = User.objects.create_user(username, email, password)
+        my_user.first_name = firstname
+        my_user.last_name = lastname
         my_user.save()
         messages.success(request, "Votre compte à été créer avec succes")
         return redirect('login')
     return render(request, 'app/register.html')
 
 
-def login(request):
+def login_(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(username=username, password=password)
         if user is not None:
             login(request, user)
-            firstname = user.firstname
+            firstname = user.first_name
             return render(request, 'app/index.html', {'firstname': firstname})
         else:
             messages.error(request, "ERREUR D'AUTHENTIFICATION")
             return redirect('login')
+    return render(request, 'app/login.html')
 
 
-def logout(request):
-    pass
+def logout_(request):
+    logout(request)
+    messages.success(request, "Vous avez été déconnecter avec succès")
+    return redirect('home')
